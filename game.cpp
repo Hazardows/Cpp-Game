2aa87cd7634e2dcde6ef2856a8a8fd0f549a16e0
Hazardows <danielde156818@gmail.com>
2025-01-29 08:33:03 -0500

    Sistema de coordenadas
    El sistema de coordenadas del juego ha sido actualizado para una mejor precisión al momento de jugar.

--- game.cpp
+++ game.cpp
@@ -9,11 +9,12 @@ bool keys[256];
 #define updateKey(key) keys[key] = ((GetAsyncKeyState(key) & 0x8000) != 0)
 
 // Configuraciones
-#define SIZEX 40
+#define SIZEX 20
 #define SIZEY 20
 #define STARTX (SIZEX / 2)
 #define STARTY (SIZEY / 2) 
-#define PLAYER_CHAR 'O'
+#define PLAYER_STRING "NIGGA"
+#define PS_SIZE 5
 #define STARTDIR 0
 // 0 -> arriba 
 // 1 -> abajo
@@ -24,12 +25,16 @@ bool keys[256];
 
 // Variables globales
 string a[SIZEY];
-int posx;
-int posy;
+int posx[PS_SIZE];
+int posy[PS_SIZE];
 int8_t goingTo;
 int curMS;
 int points;
 int foodx, foody;
+// Estados
+bool playing;
+bool ended;
+bool moving;
 
 // Funcion para imprimir el mapa
 inline void print() {
@@ -52,38 +57,35 @@ void updateScore(int &x, int &y) {
     }
 }
 
-// Mover en el eje x
-inline void movex(int& pos, int val) {
-    a[posy][pos] = ' ';
-    pos += val;
-    updateScore(pos, posy);
-    a[posy][pos] = PLAYER_CHAR;
-    print();
-}
-
-// Mover en el eje y
-inline void movey(int& pos, int val) {
-    a[pos][posx] = ' ';
-    pos += val;
-    updateScore(posx, pos);
-    a[pos][posx] = PLAYER_CHAR;
+// Mover el jugador
+inline void move(int& posx, int &posy, int valx, int valy) {
+    a[posy][posx] = ' ';
+    posx += valx * 2;
+    posy += valy;
+    if (!ended) updateScore(posx, posy);
+    a[posy][posx] = PLAYER_STRING[0];
     print();
 }
 
 inline void setMap() {
     // Preparacion del mapa
     for (int i = 1; i < SIZEY - 1; i++) {
-        a[i].resize(SIZEX, ' ');
+        a[i].resize(SIZEX * 2, ' ');
     }
 
     // Barra inferior y superior
-    a[0].resize(SIZEX, '*');
-    a[SIZEY - 1].resize(SIZEX, '*');
+    a[0].resize((SIZEX * 2) - 1, '*');
+    a[SIZEY - 1].resize((SIZEX * 2) - 1, '*');
+
+    for (int i = 1; i < SIZEX * 2; i += 2) {
+        a[0][i] = ' ';
+        a[SIZEY - 1][i] = ' ';
+    }
 
     // Barras laterales
     for (int i = 0; i < SIZEY; i++) {
         a[i][0] = '*';
-        a[i][SIZEX - 1] = '*';
+        a[i][(SIZEX * 2) - 2] = '*';
     }
 }
 
@@ -97,39 +99,56 @@ int hrandomInRange(int min, int max) {
 }
 
 void generateFood() {
-    foodx = hrandomInRange(1, SIZEX - 2);
+    foodx = hrandomInRange(1, (SIZEX * 2) - 3);
     foody = hrandomInRange(1, SIZEY - 2);
-    a[foody][foodx] = FOOD_CHAR;
-
     // Previene que la comida desaparezca :O
-    if ((foodx == posx) && (foody == posy)) generateFood();
+    if (((foodx == posx[0]) && (foody == posy[0])) || (foodx % 2 != 0)) generateFood();
+    else a[foody][foodx] = FOOD_CHAR;
 }
 
 inline bool playState() {
     // Cambiar el movimiento
-    if      (keyPressed(VK_UP))     goingTo = 0;
-    else if (keyPressed(VK_DOWN))   goingTo = 1;
-    else if (keyPressed(VK_LEFT))   goingTo = 2;
-    else if (keyPressed(VK_RIGHT))  goingTo = 3;
 
+    // TODO: Fix this
+    /*if (keyPressed(VK_SPACE)) {
+        moving = !moving;
+    }*/
+    if (keyPressed(VK_UP) || keyPressed(0x57)) {
+        if (!moving) moving = true;
+        goingTo = 0;
+    }
+    else if (keyPressed(VK_DOWN) || keyPressed(0x53)) {
+        if (!moving) moving = true;
+        goingTo = 1;
+    }
+    else if (keyPressed(VK_LEFT) || keyPressed(0x41)) {
+        if (!moving) moving = true;
+        goingTo = 2;
+    }
+    else if (keyPressed(VK_RIGHT) || keyPressed(0x44)) {
+        if (!moving) moving = true;
+        goingTo = 3;
+    }    
     // Movimiento y chequeo
-    switch (goingTo) {
-    case 0:
-        if (posy < 2) return false;
-        movey(posy, -1);
-        break;
-    case 1:
-        if (posy + 3 > SIZEY) return false;
-        movey(posy, 1);
-        break;
-    case 2:
-        if (posx < 2) return false;
-        movex(posx, -1);
-        break;
-    case 3:
-        if (posx + 3 > SIZEX) return false;
-        movex(posx, 1);
-        break;
+    if (moving) {
+        switch (goingTo) {
+        case 0:
+            if (posy[0] < 2) return false;
+            move(posx[0], posy[0], 0, -1);
+            break;
+        case 1:
+            if ((posy[0] + 3) > SIZEY) return false;
+            move(posx[0], posy[0], 0, 1);
+            break;
+        case 2:
+            if (posx[0] < 3) return false;
+            move(posx[0], posy[0], -1, 0);
+            break;
+        case 3:
+            if ((posx[0] + 5) > (SIZEX * 2)) return false;
+            move(posx[0], posy[0], 1, 0);
+            break;
+        }
     }
     return true;
 }
@@ -138,35 +157,37 @@ int main() {
     // Crear el mapa
     setMap();
 
-    // Estados
-    bool playing = true;
-    bool ended = false;
-
     // Configuraciones iniciales
-    posx = STARTX;
-    posy = STARTY;
+    posx[0] = STARTX * 2;
+    posy[0] = STARTY;
     goingTo = STARTDIR;
-    a[posy][posx] = PLAYER_CHAR;
+    a[posy[0]][posx[0]] = PLAYER_STRING[0];
     curMS = START_MS;
     points = 0;
+    playing = true;
+    ended = false;
+    moving = true;
 
     // Genera de lo que no hay en Venezuela
     generateFood();
+    print();
 
     // ks es un arreglo que contiene las teclas a ser registradas
-    int ks[]{ VK_ESCAPE, VK_SPACE, VK_UP, VK_DOWN, VK_LEFT, VK_RIGHT };
+    int ks[]{ VK_ESCAPE, VK_SPACE, VK_UP, VK_DOWN, VK_LEFT, VK_RIGHT, 0x57, 0x53, 0x44, 0x41 };
 
     // Juego
     while (!keyPressed(VK_ESCAPE)) {
         // Actualizar el registro de las teclas
-        for (int k : ks) {
-            updateKey(k);
-        }
+        for (int k : ks) updateKey(k);
 
         // Game Loop
         if (playing) {
             if (!playState()) ended = true;
-            else Sleep(curMS);
+            else {
+                /*if (!(goingTo == 0 || goingTo == 1)) Sleep(curMS);
+                else Sleep((curMS * 1.25));*/
+                Sleep(curMS);
+            }
         }
 
         // Game Over
@@ -185,11 +206,11 @@ int main() {
                 playing = true;
                 goingTo = STARTDIR;
                 curMS = START_MS;
-                movex(posx, STARTX - posx);
-                movey(posy, STARTY - posy);
+                move(posx[0], posy[0], (((STARTX * 2) - posx[0]) / 2), STARTY - posy[0]);
                 a[foody][foodx] = ' ';
                 generateFood();
                 points = 0;
+                moving = true;
             }
         }
     }
